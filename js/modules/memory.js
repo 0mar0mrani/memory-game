86804f0b1ee306e3a78f9f2053205c05b17d4e44
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-02-04 13:47:26 +0100

    Created cards in array dynamically

--- js/modules/memory.js
+++ js/modules/memory.js
@@ -1,3 +1,24 @@
 export default function Memory() {
+	const numbersOfUniqueCards = 8;
+	const numbersOfIdenticalCards = 2;
 
+	const cards = returnCreateCards();
+
+	function returnCreateCards() {
+		let newCards = [];
+
+		for (let index = 0; index < numbersOfIdenticalCards; index += 1) {
+			for (let index = 0; index < numbersOfUniqueCards; index += 1) {
+				const object = {
+					ID: index,
+					flipped: false,
+				}
+				newCards.push(object);
+			}
+		}
+
+		return newCards;	
+	}
+
+	console.log(cards);
 }
